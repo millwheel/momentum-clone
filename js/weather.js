6f138d8a0ded2c40ef99e9b6f5c29e9c5cb6032a
Bascat <79843980+millwheel@users.noreply.github.com>
2022-09-11 11:41:28 +0900

    fix the surplus
    eliminate console.log

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_key = "231f3c9e6803b51f0e04d59078346265";
 function onGeoOk(position){
     const lat = position.coords.latitude; 
     const lon = position.coords.longitude;
-    console.log(lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
     fetch(url).then(response => response.json().then(data =>{
         const weatherIconImg = document.querySelector("#weatherIcon");
